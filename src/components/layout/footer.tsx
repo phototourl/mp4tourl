b216bb756e3b6e3e4ac47cd4910893a137964770
李青松 <liqs@ytain.com>
2026-09-17 16:24:28 +0800

    Add Twelve Tools featured badge to footer.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -209,6 +209,21 @@ function FooterBadgeGroup({ ariaHidden = false }: { ariaHidden?: boolean }) {
           className="h-6 w-auto rounded-[10px] shrink-0"
         />
       </a>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <a
+        href="https://twelve.tools"
+        target="_blank"
+        rel="noreferrer"
+        className="inline-block shrink-0 no-underline transition-transform hover:scale-105"
+      >
+        <img
+          src="https://twelve.tools/badge0-white.svg"
+          alt="Featured on Twelve Tools"
+          width={148}
+          height={40}
+          className="h-6 w-auto shrink-0"
+        />
+      </a>
     </div>
   );
 }
