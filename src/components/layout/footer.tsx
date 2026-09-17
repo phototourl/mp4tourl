ff78ae67ee4e1ecb303f95cbfcf665115293738b
李青松 <liqs@ytain.com>
2026-09-17 16:33:13 +0800

    Add Good AI Tools badge to footer.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -224,6 +224,21 @@ function FooterBadgeGroup({ ariaHidden = false }: { ariaHidden?: boolean }) {
           className="h-6 w-auto shrink-0"
         />
       </a>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <a
+        href="https://goodaitools.com/ai/mp4tourl"
+        target="_blank"
+        rel="noreferrer"
+        className="inline-block shrink-0 no-underline transition-transform hover:scale-105"
+      >
+        <img
+          src="https://goodaitools.com/assets/images/badge.png"
+          alt="Good AI Tools"
+          height={54}
+          className="h-6 w-auto shrink-0"
+          loading="lazy"
+        />
+      </a>
     </div>
   );
 }
